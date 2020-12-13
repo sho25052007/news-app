71e0a103653beb70b9bfe23ba61933e42d5ba760
sho25052007 <sho25052007@gmail.com>
2020-12-13 14:00:41 +0000

    initial commit

---
+++ articles/admin.py
@@ -0,0 +1,14 @@
+# articles/admin.py
+from django.contrib import admin
+from .models import Article, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline,]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
+
